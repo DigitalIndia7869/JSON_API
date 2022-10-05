30c311f14d2682fdba1408fdf5b8690ca6693388
DigitalIndia7869 <109197838+DigitalIndia7869@users.noreply.github.com>
2022-10-05 21:12:21 +0530

    Update slider.php

--- slider.php
+++ slider.php
@@ -9,7 +9,7 @@
    },
    {
      "slider_img_url":"https://i.imgur.com/R89mtnN_d.webp?maxwidth=640&shape=thumb&fidelity=medium",
-      "url":"https://amzn.to/3CAv4jy"
+      "url":"https://amzn.to/3V6ueSy"
    },
    {
      "slider_img_url":"https://i.imgur.com/QHkbOKR_d.webp?maxwidth=640&shape=thumb&fidelity=medium",
